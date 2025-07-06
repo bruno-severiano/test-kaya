3205800ebd8ada9717ac2fc4b0495faaa2eb1e99
Bruno Severiano <severianobruno@gmail.com>
2025-07-06 23:31:39 +0800

    adding router-view and headerMain to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
-  <header class="header-main py-5">
-    <div class="container text-center">
-      <h1 class="m-0">Reports</h1>
-    </div>
-  </header>
-  <section class="section-full">
-    <div class="container">
-      <div class="row col-lg-8 offset-lg-2">
-        <ul class="list-unstyled">
-          <li
-            class="mb-4"
-            v-for="item in itemList"
-            :key="item.id"
-          >
-            <card :item="item" />
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
+  <headerMain></headerMain>
+  <router-view />
 </template>
 
 <script setup lang="ts">
-import card from './components/card.vue';
+import headerMain from './components/headerMain.vue';
+import card from './components/headerMain.vue';
 import { ref } from 'vue';
-import reports from './files/reports.json';
-
-// IMPORT JSON
-const itemList = ref(reports);
 </script>
