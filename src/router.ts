3b0a757ada4019e46f22c0711bd81326fa985553
Bruno Severiano <severianobruno@gmail.com>
2025-07-07 01:27:56 +0800

    returning to createWebHistory mode

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router' // ← use hash history
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/home.vue'
 import ReportSingle from './pages/ReportSingle.vue'
 import reports from './files/reports.json'
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 
+// HOMEPAGE and catch-all report routes
 const routes = [
   {
     path: '/',
@@ -14,11 +15,17 @@ const routes = [
     path: '/:catchAll(.*)*',
     name: 'report',
     component: ReportSingle,
-    beforeEnter: (to, from, next: NavigationGuardNext) => {
+    beforeEnter: (
+      to: RouteLocationNormalized,
+      from: RouteLocationNormalized,
+      next: NavigationGuardNext
+    ) => {
+      // Check if the requested path matches a report path in the JSON
       const match = reports.find(report => report.path === to.path)
       if (match) {
         next()
       } else {
+        // Redirect to homepage on no match (404)
         next('/')
       }
     }
@@ -26,7 +33,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
